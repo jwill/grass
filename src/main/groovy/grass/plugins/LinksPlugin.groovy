4e3c578becb585e004adbbc30b3a065746b9f2a4
James Williams <James.L.Williams@gmail.com>
2020-03-17 00:15:49 -0700

    Added youtube link creation to LinksPlugin.

--- src/main/groovy/grass/plugins/LinksPlugin.groovy
+++ src/main/groovy/grass/plugins/LinksPlugin.groovy
@@ -2,46 +2,52 @@ package grass.plugins
 
 import grass.*
 class LinksPlugin implements GrassMixin {
-	def config
+	  def config
 
-	def createLinkToPage(Page target, boolean absolute) {
-		createLinkToUrl(target.out, absolute)
-	}
+	  def createLinkToPage(Page target, boolean absolute) {
+		    createLinkToUrl(target.out, absolute)
+	  }
 
-	private createLinkToUrl(String url, boolean absolute) {
-		if (url.toLowerCase().startsWith('http')) {
-			url
-		} else {
-			def buffer = new StringBuilder()
-			if (absolute) {
-				buffer.append(config?.site?.url ?: '')
-			}
-			if (!buffer.toString().endsWith('/') && !url.startsWith('/')) {
-				buffer.append('/')
-			}
-			buffer.append(url)
-			buffer
-		}
-	}
+	  private createLinkToUrl(String url, boolean absolute) {
+		    if (url.toLowerCase().startsWith('http')) {
+			      url
+		    } else {
+			      def buffer = new StringBuilder()
+			      if (absolute) {
+				        buffer.append(config?.site?.url ?: '')
+			      }
+			      if (!buffer.toString().endsWith('/') && !url.startsWith('/')) {
+				        buffer.append('/')
+			      }
+			      buffer.append(url)
+			      buffer
+		    }
+	  }
 
-	def setupBinding(binding) {
-		binding.createLinkToPage = this.&createLinkToPage
-		binding.createLinkToUrl = this.&createLinkToUrl
+	  def setupBinding(binding) {
+		    binding.createLinkToPage = this.&createLinkToPage
+		    binding.createLinkToUrl = this.&createLinkToUrl
 
-		// dynamic dispatch based on the args to createLink()
-		binding.createLink = { target ->
-			if (target instanceof Page) {
-				binding.createLinkToPage(target, false)
-			} else if (target instanceof Map) {
-				def type = (config?.plugin?.links?.dispatch ?: []).find { target[it] }
-				if (!type) { fail("Don't know how to create a link for: ${target}") }
-				def absolute = target.containsKey('absolute') ? target.absolute : false
-				if (type) {
-					binding."createLinkTo${type.capitalize()}"(target[type], absolute)
-				}
-			} else {
-				binding.createLinkToUrl(target.toString(), false)
-			}
-		}
-	}
+		    // dynamic dispatch based on the args to createLink()
+		    binding.createLink = { target ->
+			      if (target instanceof Page) {
+				        binding.createLinkToPage(target, false)
+			      } else if (target instanceof Map) {
+				        def type = (config?.plugin?.links?.dispatch ?: []).find { target[it] }
+				        if (!type) { fail("Don't know how to create a link for: ${target}") }
+				        def absolute = target.containsKey('absolute') ? target.absolute : false
+				        if (type) {
+					          binding."createLinkTo${type.capitalize()}"(target[type], absolute)
+				        }
+			      } else {
+				        binding.createLinkToUrl(target.toString(), false)
+			      }
+		    }
+
+        binding.youtube = {Map map ->
+            """<center>
+<iframe frameborder="0" height="315" src="http://www.youtube.com/embed/$map.id" width="560"></iframe></center>
+"""
+        }
+	  }
 }
